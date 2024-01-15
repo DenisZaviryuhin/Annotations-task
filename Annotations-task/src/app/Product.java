6528329fcf26b2f9690d4521fb9b0783c6702e6a
DenisZaviryuhin <denzava30@gmail.com>
2024-01-15 10:11:57 +0200

    Add annotations

--- Annotations-task/src/app/Product.java
+++ Annotations-task/src/app/Product.java
@@ -3,8 +3,10 @@ package app;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
-
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Product {
     String name() default "unknown";
--- Annotations-task/src/app/ProductInfo.java
+++ Annotations-task/src/app/ProductInfo.java
@@ -2,8 +2,8 @@ package app;
 
 @Product(name = "orange", quota = 12)
 public class ProductInfo {
-
-    public static String getData() {
+@Product(name = "kiwi", quota = 7)
+    public String getData() {
         return "No data...";
     }
 }
